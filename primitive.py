b662a043cf0cf5b569c26c4d32c4a0eb6abfc8c9
jason <orifyuldoshev5530@gmail.com>
2026-04-16 23:44:23 +0900

    feat: learn string in python

---
+++ primitive.py
@@ -0,0 +1,28 @@
+# Pythonda hamma narsa object va primitive hisoblanadigan number ham o'zining state va method lari bor
+
+# In JAVA, variable is a name storage location
+# In Python, variable is named reference!
+
+count = 100
+count_type = type(count)
+print(f"the count: {count} and type: {count_type}")
+
+result1 = count.bit_count()  # method
+result2 = count.numerator  # state
+print(result1, result2)
+
+print("==== string ====")
+# METHODS: upper() lower() title() find() replace()
+
+course = "AI Python FullStack"
+result = type(course)
+print(f"the result (1): {result}")
+
+result = course.title()
+print(f"the result (3): {result}")
+
+result = course.upper()
+print(f"the result (3): {result}")
+
+result = course.replace("FullStack", "MasterClass")
+print(f"the result (4): {result}")
