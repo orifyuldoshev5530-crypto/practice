810d3c7d0078e27943c75e5eff6651f432dbccd9
jason <orifyuldoshev5530@gmail.com>
2026-04-20 00:44:57 +0900

    fix: python special/magic methods

--- class.about.py
+++ class.about.py
@@ -40,3 +40,47 @@ print("person1 name:", person1.name)
 # ordinary method
 person1.introduce()
 person2.say_age()
+
+
+print("==== special methods ====")
+# Python's most common special methods are below:
+# __init__ __new__ __str__ __call__ __getitem__ __eq__ __len__ ...
+
+
+class Car():
+    # state
+    description = "This class makes cars"
+
+    # constructor
+    def __new__(cls, *args):
+        print("*__new__*")
+        return super().__new__(cls)
+
+    def __init__(self, name, year):
+        self.name = name
+        self.year = year
+
+    # method
+    def start_engine(self):
+        print(f"the {self.name} started the engine!")
+
+    def stop_engine(self):
+        print(f"the {self.name} stopped the engine!")
+
+    def __str__(self):
+        return f"the {self.name} is a car made in {self.year}"
+
+    def __call__(self):
+        print("Object is called as a function!")
+        return True
+
+
+my_car = Car("Ferrari", 2025)
+my_car.start_engine()
+my_car.stop_engine()
+
+print("----")
+your_car = Car("Toyota", 2026)
+print(your_car)  # __str__ method is called when we print the object
+response = your_car()  # __call__ method is called when we call the object as a function
+print("response:", response)
