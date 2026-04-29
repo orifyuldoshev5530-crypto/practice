1441aef040d52579be204c1ecea293d1363ec229
jason <orifyuldoshev5530@gmail.com>
2026-04-29 18:17:06 +0900

    feat: Debugging process

--- debugging.py
+++ debugging.py
@@ -101,3 +101,18 @@ with Image.open("material/logo.png") as img_obj:
     resized_img = img_obj.resize((200, 200))
     resized_img.show()
     resized_img.save("material/sample.png")
+
+
+print("===== Debugging =======")
+
+
+def get_summary(*args):  # DEFINE
+    total_amount = 0
+    for a in args:
+        total_amount += a
+        return total_amount  # find the bug via debugging
+
+
+test = 100
+result = get_summary(1, 2, 3, 4, 5)  # CALL
+print("result:", result)
